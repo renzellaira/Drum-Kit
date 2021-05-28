a754f7e8c7c2413f7d403eecc4f4d78b06e86c55
renzellaira <63960644+renzellaira@users.noreply.github.com>
2021-05-28 08:57:51 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+function LargestFour(arr) {
+
+    // code goes here  
+    arr = [0, 3, 5, 8, 10];
+    arr.reverse();
+
+    for (var i = 0; i < 3; i++) {
+        var sum = 0;
+
+        sum = sum + arr[i];
+
+    }
+    return sum;
+
+}
+
+// keep this function call here 
+console.log(arr);
